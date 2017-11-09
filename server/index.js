0083faf11ab75942bddde2350053f1bc921327c5
Ryan Currie <ryan.currie@gmail.com>
2017-11-09 11:09:57 -0800

    Server file with lobby changes. #1

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const express = require('express')
 const IO = require('socket.io')
 const http = require('http')
+const Lobby = require('./scripts/lobby')
 
 const app = express()
 
@@ -11,6 +12,10 @@ const server = http.createServer(app)
 const io = IO(server)
 
 const lobbies = []
+
+if (lobbies.length === 0) {
+  lobbies.push(new Lobby())
+}
 
 io.on('connect', socket => {
   socket.on('get-lobbies', () => {
