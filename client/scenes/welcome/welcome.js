6e6b7de49decbdbb160a4da998eb7e74fa618912
Ryan Currie <ryan.currie@gmail.com>
2017-11-15 21:33:35 -0800

    Created welcome component #24

---
+++ client/scenes/welcome/welcome.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Welcome extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <h1>{`Welcome ${localStorage.getItem('username')}`}</h1>
+        </div>
+        <div className="row">
+          <div className="col text-justify">How Pex works.</div>
+        </div>
+      </div>
+    )
+  }
+}
