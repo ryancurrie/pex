2b963f2d62317791e4d825f388d7ceaf80aefb74
Ryan Currie <ryan.currie@gmail.com>
2017-11-16 15:00:03 -0800

    Updated logo in the header. #28

--- client/components/header.js
+++ client/components/header.js
@@ -4,7 +4,9 @@ const Header = () => {
   return (
     <div className="row" style={styles}>
       <div className="col text-center">
-        <span className="navbar-brand">PEX</span>
+        <span className="navbar-brand" style={logoStyle}>
+          PEX
+        </span>
       </div>
     </div>
   )
@@ -15,4 +17,14 @@ module.exports = Header
 const styles = {
   marginTop: '1em',
   marginBottom: '1.5em'
+}
+
+const logoStyle = {
+  fontWeight: '900',
+  letterSpacing: '5px',
+  fontSize: '46px',
+  color: '#007bff',
+  border: '6px solid #007bff',
+  padding: '0px 20px',
+  backgroundColor: '#f3f3f3'
 }
